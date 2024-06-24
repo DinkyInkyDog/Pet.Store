cc1e437e1307f9073b8351393f196ea0802463dd
DinkyInkyDog <142037259+DinkyInkyDog@users.noreply.github.com>
2024-06-24 05:31:15 -0400

    populate customer entity

--- src/main/java/pet/store/entity/Customer.java
+++ src/main/java/pet/store/entity/Customer.java
@@ -1,5 +1,13 @@
 package pet.store.entity;
 
-public class Customer {
+import java.util.HashSet;
+import java.util.Set;
 
+public class Customer {
+	private int customer_id;
+	private String customer_first_name;
+	private String customer_last_name;
+	private String customer_email;
+	
+	private Set<PetStore> storesShoppedAt = new HashSet<>();
 }
