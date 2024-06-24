4eb4e94776343bc34e2ac7df976b1dba46dfc66c
DinkyInkyDog <142037259+DinkyInkyDog@users.noreply.github.com>
2024-06-24 05:28:21 -0400

    populate employee entity

--- src/main/java/pet/store/entity/Employee.java
+++ src/main/java/pet/store/entity/Employee.java
@@ -1,5 +1,14 @@
 package pet.store.entity;
 
-public class Employee {
+import lombok.Data;
 
+@Data
+public class Employee {
+	private int employee_id;
+	private String employee_first_name;
+	private String employee_last_name;
+	private int employee_phone;
+	private String employee_job_title;
+	
+	private PetStore petstore;
 }
