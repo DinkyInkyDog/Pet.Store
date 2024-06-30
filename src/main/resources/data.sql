7763e19bfdfa6a9b440b71ec21dc673e05f6037f
DinkyInkyDog <142037259+DinkyInkyDog@users.noreply.github.com>
2024-06-29 22:07:16 -0400

    add ERD

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,3 @@
 INSERT INTO customer (customer_first_name, customer_last_name, customer_email) 
 VALUES ('Luna', 'Moonberry', 'LuneyBerry@gmail.com');
+INSERT INTO customer (customer_first_name, customer_last_name, customer_email) VALUES ('Cami', 'Oosdyke', 'CamiOosdyke@gmail.com');
